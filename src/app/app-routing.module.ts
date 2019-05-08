6bcdb40f3f919990487330c0ea43781daf3a0f85
Mayank Khanna <khanna98@outlook.com>
2019-05-09 00:48:09 +0530

    Resume Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {ProjectsComponent} from './projects/projects.component';
-import {ContactComponent} from './contact/contact.component';
-import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'home',component: HomeComponent},
-  { path: 'projects',component: ProjectsComponent},
-  { path: 'contact',component: ContactComponent},
-  { path: 'resume',component: ResumeComponent},
-  { path: '',redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
